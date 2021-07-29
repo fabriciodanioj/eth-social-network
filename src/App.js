4edddbe330fd157ddeed5c2701bea7b0ce3acad3
fabriciodanioj <fabriciodanioj@gmail.com>
2021-07-28 22:55:56 -0300

    feat: add web3 to react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from "react";
 
+import Web3 from "web3";
+
+import { Container } from "./styles";
+
 function App() {
+  const [userAddress, setUserAddress] = React.useState("");
+
+  async function loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
+  async function loadBlockchainData() {
+    const accounts = await window.web3.eth.getAccounts();
+
+    setUserAddress(accounts[0]);
+  }
+
+  React.useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
+
   return (
-    <div>
-      <a
-        className="App-link"
-        href="http://www.dappuniversity.com/bootcamp"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LEARN BLOCKCHAIN{" "}
-        <u>
-          <b>NOW! </b>
-        </u>
-      </a>
-    </div>
+    <Container>
+      <h1>{userAddress}</h1>
+    </Container>
   );
 }
 
